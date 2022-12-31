93d42fc5e4d5e67c05e0c7185da52fa9e0a027b7
Jimmy Hu <jimmyhu.coding@gmail.com>
2023-01-01 00:42:54 +0800

    done

--- src/App.tsx
+++ src/App.tsx
@@ -36,169 +36,161 @@ const ToolButton = styled.button`
   border: none;
 `;
 
-type Content = { content: string; color: string; type: 'content' | 'markup' };
+type Content = { text: string; color: string; type: 'content' | 'markup' };
 
 function App() {
   const [contents, setContents] = useState<Content[]>([
     {
-      content: lorem,
+      text: lorem || '1234567890',
       color: '#fff',
       type: 'content',
     },
   ]);
-  const [relatedContents, setRelatedContents] = useState<
-    typeof contents | null
-  >(null);
-  const [markupStart, setMarkupStart] = useState<number | null>(null);
+  const [showTools, setShowTools] = useState(false);
   const [mousePosition, setMousePosition] = useState<{
     x: number;
     y: number;
   } | null>(null);
-  const [hasConflict, setHasConflict] = useState(false);
-  const [dealWithConflict, setDealWithConflict] = useState<
-    'merge' | 'keepNew' | null
-  >(null);
-  const [showTools, setShowTools] = useState(false);
   const [confirm, setConfirm] = useState(false);
 
   const reset = useCallback(() => {
-    setRelatedContents(null);
-    setMarkupStart(null);
-    setHasConflict(false);
-    setDealWithConflict(null);
     setShowTools(false);
-    setConfirm(false);
     setMousePosition(null);
+    setConfirm(false);
   }, []);
 
   const handleMouseUp = useCallback(
     (e: React.MouseEvent<HTMLElement, MouseEvent>) => {
-      const selection = window.getSelection();
-      console.log({ e, selection });
-      if (markupStart === null) {
-        return;
-      }
+      const selection = getSelection();
       if (selection?.toString()) {
-        setMousePosition({ x: e.clientX, y: e.clientY });
-        const index = Number(e.currentTarget.dataset.contentIndex);
-        const currentRelatedContents = contents.slice(markupStart, index + 1);
-        console.log({ markupStart, index, contents });
-        setRelatedContents(currentRelatedContents);
-        console.log({ currentRelatedContents });
-        const hasConflict = currentRelatedContents.some(
-          (item) => item.type === 'markup'
-        );
-        if (hasConflict) {
-          setHasConflict(true);
-        }
+        setMousePosition({
+          x: e.clientX,
+          y: e.clientY,
+        });
         setShowTools(true);
       } else {
         reset();
       }
     },
-    [contents, markupStart, reset]
+    [reset]
   );
 
-  const handleConflict = useCallback((method: typeof dealWithConflict) => {
-    setDealWithConflict(method);
-  }, []);
-
-  const finishMarkup = useCallback(() => {
-    if (!relatedContents) {
-      return;
-    }
+  const handleConfirm = useCallback(() => {
     const selection = getSelection();
-    if (!selection) {
-      return;
-    }
     setContents((prev) => {
-      let newContents = [...prev];
-      if (hasConflict) {
-      } else {
-        const currentContent = relatedContents[0];
-        console.log({
-          currentContent,
-          relatedContents,
-          newContents,
-        });
-        const newMarkup: Content = {
-          content: selection.toString(),
-          color: 'yellow',
-          type: 'markup',
-        };
-        const beforeMarkupContent: Content = {
-          type: 'content',
-          content: currentContent?.content.slice(0, selection.anchorOffset),
-          color: '#fff',
-        };
-        const afterMarkupContent: Content = {
-          content: currentContent?.content.slice(selection.focusOffset),
-          color: '#fff',
-          type: 'content',
-        };
-        console.log({
-          beforeMarkupContent,
-          newMarkup,
-          afterMarkupContent,
-        });
-        newContents.splice(
-          markupStart || 0,
-          relatedContents.length,
-          beforeMarkupContent,
-          newMarkup,
-          afterMarkupContent
-        );
+      if (!selection?.toString()) {
+        return prev;
       }
+      let newContents = [...prev];
+      let [startContentIndex, endContentIndex] = [
+        selection?.anchorNode?.parentElement?.id.split('-')[1],
+        selection?.focusNode?.parentElement?.id.split('-')[1],
+      ].map((index) => Number(index));
+      let startOffset = selection?.anchorOffset;
+      let endOffset = selection?.focusOffset;
+
+      if (startContentIndex > endContentIndex) {
+        [startContentIndex, endContentIndex] = [
+          endContentIndex,
+          startContentIndex,
+        ];
+        [startOffset, endOffset] = [endOffset, startOffset];
+      }
+      if (startContentIndex === endContentIndex && startOffset > endOffset) {
+        [startOffset, endOffset] = [endOffset, startOffset];
+      }
+      endOffset += newContents
+        .slice(startContentIndex, endContentIndex)
+        .map((content) => content.text)
+        .join('').length;
+      const relatedContents = newContents.slice(
+        startContentIndex,
+        endContentIndex + 1
+      );
+      const relatedText = relatedContents.reduce((acc, cur) => {
+        return acc + cur.text;
+      }, '');
+
+      const before: Content = {
+        text: relatedText.slice(0, startOffset),
+        color: '#fff',
+        type: 'content',
+      };
+      const after: Content = {
+        text: relatedText.slice(endOffset),
+        color: '#fff',
+        type: 'content',
+      };
+      const selected: Content = {
+        text: relatedText.slice(startOffset, endOffset),
+        color: '#ff0',
+        type: 'markup',
+      };
+      newContents.splice(
+        startContentIndex,
+        relatedContents.length,
+        before,
+        selected,
+        after
+      );
+      const groupContents = newContents.reduce((acc, cur) => {
+        const lastGroupType = acc.at(-1)?.[0]?.type;
+        if (lastGroupType === cur.type) {
+          acc.at(-1)?.push(cur);
+        } else {
+          acc.push([cur]);
+        }
+        return acc;
+      }, [] as Content[][]);
+      newContents = groupContents.map((group) =>
+        group.reduce(
+          (acc, cur) => {
+            return {
+              text: acc.text + cur.text,
+              color: cur.color,
+              type: cur.type,
+            };
+          },
+          {
+            text: '',
+            color: '#fff',
+            type: 'content',
+          }
+        )
+      );
       return newContents;
     });
     reset();
-  }, [hasConflict, markupStart, relatedContents, reset]);
+  }, [reset]);
+
+  useEffect(() => {
+    if (confirm) {
+      handleConfirm();
+    }
+  }, [confirm, handleConfirm]);
+
+  useEffect(() => {
+    getSelection()?.empty();
+  }, [contents]);
 
   console.log({ contents });
 
-  useEffect(() => {
-    if (!relatedContents) {
-      return;
-    }
-    if (hasConflict && !dealWithConflict) {
-      return;
-    }
-    if (confirm) {
-      finishMarkup();
-    }
-  }, [confirm, dealWithConflict, finishMarkup, hasConflict, relatedContents]);
-
   return (
-    <Main>
+    <Main onMouseUp={(e) => handleMouseUp(e)}>
       <Article>
         {contents.map((item, index) => (
           <span
+            id={`content-${index}`}
             key={index}
             style={{ background: item.color }}
-            onMouseDown={() => setMarkupStart(index)}
-            onMouseUp={(e) => handleMouseUp(e)}
-            data-content-index={index}
           >
-            {item.content}
+            {item.text}
           </span>
         ))}
       </Article>
       {showTools && (
         <Tools top={mousePosition?.y || 0} left={mousePosition?.x || 0}>
-          {hasConflict && (
-            <>
-              <ToolButton onClick={() => handleConflict('merge')}>
-                Merge
-              </ToolButton>
-              <ToolButton onClick={() => handleConflict('keepNew')}>
-                Keep New
-              </ToolButton>
-              <ToolButton onClick={reset}>Cancel</ToolButton>
-            </>
-          )}
-          {!hasConflict && (
-            <ToolButton onClick={() => setConfirm(true)}>confirm</ToolButton>
-          )}
+          <ToolButton onClick={() => setConfirm(true)}>confirm</ToolButton>
         </Tools>
       )}
     </Main>
